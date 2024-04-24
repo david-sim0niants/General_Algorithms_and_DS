d79492bc47838a3d4ae16bf851bf0f68569fd747
David <david.simonyants@gmail.com>
2024-04-24 13:11:32 +0400

    CMake and gtest setup.

---
+++ test/huffman_coding.cpp
@@ -0,0 +1,24 @@
+#include "huffman_coding.h"
+
+#include <gtest/gtest.h>
+
+
+class HuffmanCodingTest : public ::testing::Test {
+protected:
+    void SetUp() override;
+    void TearDown() override;
+};
+
+
+void HuffmanCodingTest::SetUp()
+{
+}
+
+void HuffmanCodingTest::TearDown()
+{
+}
+
+
+TEST_F(HuffmanCodingTest, Compression)
+{
+}
