1d1fd10dcd5d376856ebd287ec22429c7a121ab4
David <david.simonyants@gmail.com>
2024-04-25 14:17:58 +0400

    Added more comments for Huffman coding.

--- include/huffman_coding.h
+++ include/huffman_coding.h
@@ -1,5 +1,9 @@
 #pragma once
 
+/*
+ * Huffman coding: Huffman Tree, Codeword table, Encoder and Decoder
+ * */
+
 #include <cstddef>
 #include <variant>
 #include <optional>
@@ -12,43 +16,51 @@
 #include "error.h"
 
 
+/* Class representing the Huffman tree. */
 template<typename Symbol>
 class HuffmanTree {
 private:
     struct Branches {
+        // Left and right subtrees which are stored as unique pointers.
         std::unique_ptr<HuffmanTree> left, right;
     };
 
 public:
+    /* Construct a leaf (symbol) Huffman tree node. */
     HuffmanTree(size_t freq, const Symbol& symbol)
         : freq(freq), content(symbol)
     {}
 
+    /* Construct a non-leaf Huffman tree node by merging left and right subtrees. */
     HuffmanTree(std::unique_ptr<HuffmanTree>&& left, std::unique_ptr<HuffmanTree>&& right)
         :   freq((left ? left->freq : 0) + (right ? right->freq : 0)),
             content(Branches {std::move(left), std::move(right)})
     {
         if (!get_left() || !get_right())
-            throw Error<HuffmanTree>("A non-symbol HuffmanTree node must have exactly two subtrees.");
+            throw Error<HuffmanTree>("A non-leaf HuffmanTree node must have exactly two subtrees.");
     }
 
+    /* Get frequency of the node. */
     inline size_t get_freq() const
     {
         return freq;
     }
 
+    /* Get symbol if the node is a leaf node (contains symbol); otherwise, nothing. */
     std::optional<Symbol> get_symbol() const
     {
         const Symbol *const p_symbol = std::get_if<Symbol>(&content);
         return p_symbol ? std::optional {*p_symbol} : std::nullopt;
     }
 
+    /* Get left subtree if the node is a non-leaf node; otherwise, nothing. */
     inline HuffmanTree *get_left() const
     {
         const Branches *const p_branches = get_branches();
         return p_branches ? p_branches->left.get() : nullptr;
     }
 
+    /* Get right subtree if the node is a non-leaf node; otherwise, nothing. */
     HuffmanTree *get_right() const
     {
         const Branches *const p_branches = get_branches();
@@ -61,8 +73,10 @@ private:
         return std::get_if<Branches>(&content);
     }
 
-    size_t freq;
-    std::variant<Symbol, Branches> content;
+    size_t freq; /* The frequency of the node: for a leaf (symbol) node, it stores
+                    the frequency of the symbol; otherwise, it stores the cumulative
+                    frequency of all leaf (symbol) nodes descending from this node. */
+    std::variant<Symbol, Branches> content; /* Either a symbol for a leaf node, or a left and right subtree pair. */
 };
 
 
@@ -70,17 +84,19 @@ template<typename Symbol>
 std::unique_ptr<HuffmanTree<Symbol>> build_huffman_tree(const std::unordered_map<Symbol, size_t>& sym_freq);
 
 
+/* Build an optimal Huffman tree for the provided stream of symbols. */
 template<typename SymbolIt>
 std::unique_ptr<HuffmanTree<std::iter_value_t<SymbolIt>>> build_huffman_tree(SymbolIt begin, SymbolIt end)
 {
     using Symbol = std::iter_value_t<SymbolIt>;
     std::unordered_map<Symbol, size_t> sym_freq;
     for (auto it = begin; it != end; ++it)
-        ++sym_freq[*it];
+        ++sym_freq[*it]; // measure the frequency of each appearing symbol
     return build_huffman_tree(sym_freq);
 }
 
 
+/* Build Huffman tree from the provided symbol frequency map. */
 template<typename Symbol>
 std::unique_ptr<HuffmanTree<Symbol>> build_huffman_tree(const std::unordered_map<Symbol, size_t>& sym_freq)
 {
@@ -112,6 +128,7 @@ std::unique_ptr<HuffmanTree<Symbol>> build_huffman_tree(const std::unordered_map
 }
 
 
+/* Type representing Huffman table that maps each symbol to its codeword. */
 template<typename Symbol>
 using HuffmanTable = std::unordered_map<Symbol, std::string>;
 
@@ -120,6 +137,7 @@ template<typename Symbol>
 void __build_huffman_table(const HuffmanTree<Symbol> *tree, HuffmanTable<Symbol>& table, std::string& codeword);
 
 
+/* Build Huffman table by traversing through the tree. */
 template<typename Symbol>
 HuffmanTable<Symbol> build_huffman_table(const HuffmanTree<Symbol> *tree)
 {
@@ -133,6 +151,7 @@ HuffmanTable<Symbol> build_huffman_table(const HuffmanTree<Symbol> *tree)
 }
 
 
+/* Internal Huffman table builder function that actually does the traversal through the tree. */
 template<typename Symbol>
 void __build_huffman_table(const HuffmanTree<Symbol> *tree, HuffmanTable<Symbol>& table, std::string& codeword)
 {
@@ -143,30 +162,34 @@ void __build_huffman_table(const HuffmanTree<Symbol> *tree, HuffmanTable<Symbol>
         return;
     }
 
-    codeword.push_back('0');
+    codeword.push_back('0'); // left path corresponds to 0
     __build_huffman_table(tree->get_left(), table, codeword);
-    codeword.back() = '1';
+    codeword.back() = '1'; // right path corresponds to 1
     __build_huffman_table(tree->get_right(), table, codeword);
     codeword.pop_back();
 }
 
 
-
+/* Huffman encoder class that deals with compression of the supplied data. */
 template<typename Symbol>
 class HuffmanEncoder {
 public:
+    /* Construct Huffman encoder from the table. */
     HuffmanEncoder(std::ostream& os, HuffmanTable<Symbol>&& table)
         : os(os), table(std::move(table))
     {}
 
+    /* Construct Huffman encoder from the tree. */
     HuffmanEncoder(std::ostream& os, const std::unique_ptr<HuffmanTree<Symbol>> &tree)
         : os(os), table(build_huffman_table(tree.get()))
     {}
 
+    /* Construct Huffman encoder from the tree. */
     HuffmanEncoder(std::ostream& os, const HuffmanTree<Symbol> *tree)
         : os(os), table(build_huffman_table(tree))
     {}
 
+    /* Construct Huffman encoder with optimal codeword table for the provided symbol stream. */
     template<typename SymbolIt>
     HuffmanEncoder(std::ostream& os, SymbolIt begin, SymbolIt end)
         : os(os)
@@ -180,6 +203,7 @@ public:
         finalize();
     }
 
+    /* Encode one symbol and write to the output stream. */
     void put_sym(const Symbol& symbol)
     {
         for (char c : table[symbol]) {
@@ -192,6 +216,7 @@ public:
         }
     }
 
+    /* Encode symbol stream and write to the output stream. */
     template<typename SymbolIt>
     void write_syms(SymbolIt begin, SymbolIt end)
     {
@@ -199,27 +224,32 @@ public:
             put_sym(*it);
     }
 
+    /* Finalize the stream by writing last unwritten bits followed by zero bits. */
     void finalize()
     {
-        if (off_left)
+        if (off_left & 7)
             os.put(unwritten_bits << off_left);
+        off_left = 8;
     }
 
 private:
-    std::ostream& os;
-    HuffmanTable<Symbol> table;
-    int off_left = 8;
-    char unwritten_bits = 0;
+    std::ostream& os; /* The output stream where the encoded data is written to. */
+    HuffmanTable<Symbol> table; /* Huffman codeword table. */
+    int off_left = 8; /* Offset left to fill to send 8 bits to the stream. */
+    char unwritten_bits = 0; /* Unwritten bits of last encoded data. */
 };
 
 
+/* Huffman decoder class that deals with decompression of the compressed data. */
 template<typename Symbol>
 class HuffmanDecoder {
 public:
+    /* Construct Huffman decoder from the tree (the tree must be alive throughout the lifetime of the decoder). */
     HuffmanDecoder(std::istream& is, HuffmanTree<Symbol>& tree)
         : is(is), tree(tree)
     {}
 
+    /* Read one symbol from the input stream. */
     std::optional<Symbol> get_sym()
     {
         std::optional<Symbol> opt_symbol;
@@ -236,6 +266,7 @@ public:
         return opt_symbol;
     }
 
+    /* Read multiple symbols from the input stream. */
     template<typename SymbolIt>
     void read_syms(SymbolIt begin, SymbolIt end)
     {
@@ -247,6 +278,7 @@ public:
         }
     }
 
+    /* Reset the decoder state. */
     void reset()
     {
         curr = &tree;
@@ -255,14 +287,15 @@ public:
     }
 
 private:
-    std::istream& is;
-    HuffmanTree<Symbol>& tree;
-    HuffmanTree<Symbol> *curr = &tree;
-    int off_left = 0;
-    char unread_bits = 0;
+    std::istream& is; /* The input stream from which the encoded data is read. */
+    HuffmanTree<Symbol>& tree; /* Reference to the Huffman tree. */
+    HuffmanTree<Symbol> *curr = &tree; /* Last visited Huffman tree node during decoding. */
+    int off_left = 0; /* Offset of unread bits. */
+    char unread_bits = 0; /* Unread bits. */
 };
 
 
+/* Huffman basic string encoder designed for compressing character streams. */
 template<typename Char>
 class HuffmanBasicStringEncoder : public HuffmanEncoder<Char> {
 public:
@@ -334,6 +367,7 @@ using HuffmanStringEncoder = HuffmanBasicStringEncoder<char>;
 using HuffmanWStringEncoder = HuffmanBasicStringEncoder<wchar_t>;
 
 
+/* Huffman basic string decoder designed for decompressing compressed character streams. */
 template<typename Char>
 class HuffmanBasicStringDecoder : HuffmanDecoder<Char> {
 public:
